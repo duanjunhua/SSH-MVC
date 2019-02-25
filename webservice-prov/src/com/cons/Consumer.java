3144fb18f32b9cc8ef5b10e8747c1f5e1b4fe0d2
duanjunhua <1585115894@qq.com>
2019-02-25 16:14:16 +0800

    WebService Demo

--- webservice-prov/src/com/cons/Consumer.java
+++
@@ -1,9 +0,0 @@
-package com.cons;
-
-public class Consumer {
-
-	public static void main(String[] args) {
-//		Function fu =new FunctionService().getFunctionPort();  
-//	    String str=fu.transWords("Let's Get Heck Out Of Here!");  
-	}
-}
